8920661d756e7b6fbf16282285ce5d45a77af5cf
nekto-nekto <nekto@0city.ru>
2019-10-30 14:20:41 +0300

    fix check on old browser (window.WebAssembly disabled in Tor — javascript.options.wasm;false)

--- clientScripts/loader.js
+++ clientScripts/loader.js
@@ -30,7 +30,78 @@
 	}
 
 	// Really old browser. Run in noscript mode.
-	if (!window.WebAssembly) {
+	// Check for browser compatibility by trying to detect some ES6 features
+	function check(func) {
+		try {
+			return eval('(function(){' + func + '})()')
+		}
+		catch (e) {
+			return false
+		}
+	}
+	var es6Tests = [
+		// Arrow functions
+		'return (()=>5)()===5;',
+
+		// Block scoped const
+		'"use strict";  const bar = 123; {const bar = 456;} return bar===123;',
+
+		// Block-scoped let
+		'"use strict"; let bar = 123;{ let bar = 456; }return bar === 123;',
+
+		// Computed object properties
+		"var x='y';return ({ [x]: 1 }).y === 1;",
+
+		// Shorthand object properties
+		"var a=7,b=8,c={a,b};return c.a===7 && c.b===8;",
+
+		// Template strings
+		'var a = "ba"; return `foo bar${a + "z"}` === "foo barbaz";',
+
+		// for...of
+		'var arr = [5]; for (var item of arr) return item === 5;',
+
+		// Spread operator
+		'return Math.max(...[1, 2, 3]) === 3',
+
+		// Class statement
+		'"use strict"; class C {}; return typeof C === "function"',
+
+		// Super call
+		'"use strict"; var passed = false;'
+		+ 'class B {constructor(a) {  passed = (a === "barbaz")}};'
+		+ 'class C extends B {constructor(a) {super("bar" + a)}};'
+		+ 'new C("baz"); return passed;',
+
+		// Default parameters
+		'return (function (a = 1, b = 2) { return a === 3 && b === 2; }(3));',
+
+		// Destructuring declaration
+		'var [a,,[b],c] = [5,null,[6]];return a===5 && b===6 && c===undefined',
+
+		// Parameter destructuring
+		'return function([a,,[b],c]){return a===5 && b===6 && c===undefined;}'
+		+ '([5,null,[6]])',
+
+		// Generators
+		'function * generator(){yield 5; yield 6};'
+		+ 'var iterator = generator();'
+		+ 'var item = iterator.next();'
+		+ 'var passed = item.value === 5 && item.done === false;'
+		+ 'item = iterator.next();'
+		+ 'passed &= item.value === 6 && item.done === false;'
+		+ 'item = iterator.next();'
+		+ 'passed &= item.value === undefined && item.done === true;'
+		+ 'return passed;'
+	]
+	for (var i = 0; i < es6Tests.length; i++) {
+		if (!check(es6Tests[i])) {
+			window.legacy = true
+			break
+		}
+	}
+	//if (!window.WebAssembly) {
+	if (window.legacy) {
 		var ns = document.getElementsByTagName("noscript");
 
 		while (ns.length) { // Collection is live and changes with DOM updates
