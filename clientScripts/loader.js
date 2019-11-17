73f57381f28a0426a28e976f53cdaf789607b10a
nekto-nekto <nekto@0city.ru>
2019-11-17 17:03:43 +0300

    add append polyfill

--- clientScripts/loader.js
+++ clientScripts/loader.js
@@ -1,5 +1,29 @@
 // Selects and loads the client files and polyfills, if any. Use only ES5.
 
+Document.prototype.append || (function (arr) { // Source: https://github.com/jserz/js_piece/blob/master/DOM/ParentNode/append()/append().md
+	arr.forEach(function (item) {
+		if (item.hasOwnProperty('append')) {
+			return;
+		}
+		Object.defineProperty(item, 'append', {
+			configurable: true,
+			enumerable: true,
+			writable: true,
+			value: function append() {
+				var argArr = Array.prototype.slice.call(arguments),
+					docFrag = document.createDocumentFragment();
+				argArr.forEach(function (argItem) {
+					var isNode = argItem instanceof Node;
+					docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
+				});
+				
+				this.appendChild(docFrag);
+			}
+		});
+	});
+})([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+
+
 (function () {
 	// Check if the client is an automated crawler
 	var isBot,
