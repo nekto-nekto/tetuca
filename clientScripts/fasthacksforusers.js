efa266b02f8c723bd1633f7271312d8e9ae43de5
nekto-nekto <nekto@0city.ru>
2019-12-03 14:57:52 +0300

    temporarily disable hide mode

--- clientScripts/fasthacksforusers.js
+++ clientScripts/fasthacksforusers.js
@@ -1,30 +1,34 @@
+function ready(fn) { if (document.readyState != 'loading'){ fn(); } else if (document.addEventListener) { document.addEventListener('DOMContentLoaded', fn); } else { document.attachEvent('onreadystatechange', function() { if (document.readyState != 'loading') fn(); }); } };
+
 (function () {
 
 	var hiddenboardsRaw = localStorage.getItem("hiddenboards");
 	if (hiddenboardsRaw !== null) {
 		var hiddenboards = hiddenboardsRaw.split(",");
-		var hiddenboards_mode = localStorage.getItem("hiddenboards_mode");
+		/*var hiddenboards_mode = localStorage.getItem("hiddenboards_mode");
 		if (hiddenboards_mode === null) {
 			hiddenboards_mode = "deleted";
-		}
-		var boards = document.querySelectorAll(".board");
-		for (var b = 0; b < boards.length; b++) {
-			var boardname = boards[b].textContent;
-			if (hiddenboards.includes(boardname)) {
-				var header = boards[b].parentNode;
-				if (hiddenboards_mode === "hide") {
-					var control = header.querySelector(".control");
-					control.click();
-					var hideitem = control.querySelector('.popup-menu [data-id="hide"]');
-					hideitem.click();
-				} else {
-					var article = header.parentNode;
-					var section = article.parentNode;
-					section.classList.add("deleted");
-					section.classList.add("by-user");
+		}*/
+		ready(function() {
+			var boards = document.querySelectorAll(".board");
+			for (var b = 0; b < boards.length; b++) {
+				var boardname = boards[b].textContent;
+				if (hiddenboards.includes(boardname)) {
+					var header = boards[b].parentNode;
+					/*if (hiddenboards_mode === "hide") {
+						var control = header.querySelector(".control");
+						control.click();
+						var hideitem = control.querySelector('.popup-menu [data-id="hide"]');
+						hideitem.click();
+					} else {*/
+						var article = header.parentNode;
+						var section = article.parentNode;
+						section.classList.add("deleted");
+						section.classList.add("by-user");
+					//}
 				}
 			}
-		}
+		});
 	}
 
 })();
