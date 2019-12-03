4d8288f09d8b54863b790705fc0c123de64635cb
nekto-nekto <nekto@0city.ru>
2019-12-03 14:44:44 +0300

    fix GulpUglifyError: unable to minify JavaScript

--- clientScripts/fasthacksforusers.js
+++ clientScripts/fasthacksforusers.js
@@ -1,27 +1,27 @@
 (function () {
 
-	let hiddenboardsRaw = localStorage.getItem("hiddenboards");
+	var hiddenboardsRaw = localStorage.getItem("hiddenboards");
 	if (hiddenboardsRaw !== null) {
-		let hiddenboards = hiddenboardsRaw.split(",");
-		let hiddenboards_mode = localStorage.getItem("hiddenboards_mode");
+		var hiddenboards = hiddenboardsRaw.split(",");
+		var hiddenboards_mode = localStorage.getItem("hiddenboards_mode");
 		if (hiddenboards_mode === null) {
 			hiddenboards_mode = "deleted";
 		}
 		var boards = document.querySelectorAll(".board");
-		for (let b = 0; b < boards.length; b++) {
-			let boardname = boards[b].textContent;
+		for (var b = 0; b < boards.length; b++) {
+			var boardname = boards[b].textContent;
 			if (hiddenboards.includes(boardname)) {
-				let header = boards[b].parentNode;
+				var header = boards[b].parentNode;
 				if (hiddenboards_mode === "hide") {
-					let control = header.querySelector(".control");
+					var control = header.querySelector(".control");
 					control.click();
-					let hideitem = control.querySelector('.popup-menu [data-id="hide"]');
+					var hideitem = control.querySelector('.popup-menu [data-id="hide"]');
 					hideitem.click();
 				} else {
-					let article = header.parentNode;
-					let section = article.parentNode;
+					var article = header.parentNode;
+					var section = article.parentNode;
 					section.classList.add("deleted");
-					section.classList.add("by-user")
+					section.classList.add("by-user");
 				}
 			}
 		}
