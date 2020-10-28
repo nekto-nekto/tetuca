af479c72b3455434d65de6b03f7dffa3b18241f3
nekto-nekto <55918742+nekto-nekto@users.noreply.github.com>
2020-10-28 13:40:30 +0300

    forcing hide hidden post on load
    Crutch for moronic architecture

--- clientScripts/fasthacksforusers.js
+++ clientScripts/fasthacksforusers.js
@@ -2,6 +2,7 @@ function ready(fn) { if (document.readyState != 'loading'){ fn(); } else if (doc
 
 (function () {
 
+	// hide boards
 	var hiddenboardsRaw = localStorage.getItem("hiddenboards");
 	if (hiddenboardsRaw !== null) {
 		var hiddenboards = hiddenboardsRaw.split(",");
@@ -31,4 +32,73 @@ function ready(fn) { if (document.readyState != 'loading'){ fn(); } else if (doc
 		});
 	}
 
+	// forcing hide hidden post on load
+	
+	window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.OIndexedDB || window.msIndexedDB, IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.OIDBTransaction || window.msIDBTransaction;
+	var dbVersion = 12;
+	var dbName = "meguca";
+	var request = indexedDB.open(dbName);
+	var db, transaction, objectStore;
+	var hiddenPostsSelector = "";
+	var hidePostInterval;
+	var dbExists = true;
+	request.onupgradeneeded = function(e) {
+		if(request.result.version===1){
+			dbExists = false;
+			window.indexedDB.deleteDatabase(dbName);
+		}
+	}
+	request.onsuccess = function (event) {
+		if (dbExists) {
+			db = request.result;
+			db.onerror = function (event) {
+				console.error("Error accessing IndexedDB database");
+			}
+			transaction = db.transaction(["hidden"], "readonly");
+			transaction.onerror = function(event) {
+				console.error(event);
+			};
+			objectStore = transaction.objectStore("hidden");
+			request = objectStore.getAll();
+			request.onsuccess = function(event) {
+				var hiddenPosts = event.target.result;
+				for (var i = 0; i < hiddenPosts.length; i++) {
+					var hiddenPost = hiddenPosts[i];
+					if (hiddenPost.op == hiddenPost.id) {
+						hiddenPostsSelector += 'section.index-thread[data-id="' + hiddenPost.id + '"] article';
+					} else {
+						hiddenPostsSelector += 'article[id="p$' + hiddenPost.id + '"]';
+					}
+					if (i < hiddenPosts.length-1) {
+						hiddenPostsSelector += ", ";
+					}
+				}
+				var posthideFunc = function() {
+					if (hiddenPostsSelector !== "") {
+						var postforhide = document.querySelectorAll(hiddenPostsSelector);
+						postforhide.forEach(function(element) {
+							element.classList.add("hidden");
+						});
+					}
+				}
+				posthideFunc();
+				hidePostInterval = setInterval(posthideFunc, 50);
+			}
+		} else {
+			request.result.close();
+		}
+	}
+	request.onerror = function(event) {
+		console.error("error", event);
+	}
+
+	document.addEventListener("readystatechange", function (event) {
+		if (event.target.readyState === "complete") {
+			clearInterval(hidePostInterval);
+			var clearIntervalFunc = setInterval(function() {
+				clearInterval(hidePostInterval);
+				clearInterval(clearIntervalFunc);
+			}, 100);
+		}
+	});
 })();
